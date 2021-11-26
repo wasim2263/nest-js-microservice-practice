f657299e501390979b34f8dffc47960275340b03
abdullah.wasim <abdullahwasim42@gmail.com>
2021-11-27 00:24:38 +0600

    controller modified

--- grpc-test-1/src/app.controller.ts
+++ grpc-test-1/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { AppService } from './app.service';
 import { Body, Logger, Post, Controller, Get } from '@nestjs/common';
-
+import {MessagePattern} from '@nestjs/microservices'
 @Controller()
 export class AppController {
   private logger = new Logger();
@@ -15,6 +15,13 @@ export class AppController {
   }
   @Post('full-name')
   async getFullName(@Body("data") data: string[],
+  ) {
+    this.logger.log('adding', data[0]);
+    this.logger.log('adding', data[1]);
+    return this.appService.getFullName(data);
+  }
+  @MessagePattern('full-name')
+  async getTCPFullName( data: string[],
   ) {
     this.logger.log('adding', data[0]);
     this.logger.log('adding', data[1]);
