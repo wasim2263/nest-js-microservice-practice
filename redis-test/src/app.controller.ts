bf5c17506c27d9cebe2951a23501a4359ba6bdc7
abdullah.wasim <abdullahwasim42@gmail.com>
2021-12-03 01:29:07 +0600

    redis-test added for microservice practice

---
+++ redis-test/src/app.controller.ts
@@ -0,0 +1,30 @@
+import { AppService } from './app.service';
+import { Body, Logger, Post, Controller, Get } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+@Controller()
+export class AppController {
+  private logger = new Logger();
+  constructor(private readonly appService: AppService) {}
+
+  // constructor() {}
+
+  // @Get('test')
+  // getHello(): string {
+  //   console.log('new');
+
+  //   return this.appService.getHello();
+  // }
+  // @Post('full-name')
+  // async getFullName(@Body("data") data: string[],
+  // ) {
+  //   this.logger.log('adding', data[0]);
+  //   this.logger.log('adding', data[1]);
+  //   return this.appService.getFullName(data);
+  // }
+  @MessagePattern('full-name')
+  async getTCPFullName(data: string[]) {
+    this.logger.log('adding', data[0]);
+    this.logger.log('adding', data[1]);
+    return this.appService.getFullName(data);
+  }
+}
