e89b1f2405a7ee7e2346a79c3e8d1be72ef61332
abdullah.wasim <abdullahwasim42@gmail.com>
2021-12-04 18:35:07 +0600

    porto file modified and controller modified

--- grpc-test/src/app.controller.ts
+++ grpc-test/src/app.controller.ts
@@ -6,21 +6,7 @@ export class AppController {
   private logger = new Logger();
   constructor(private readonly appService: AppService) {}
 
-  // constructor() {}
 
-  // @Get('test')
-  // getHello(): string {
-  //   console.log('new');
-
-  //   return this.appService.getHello();
-  // }
-  // @Post('full-name')
-  // async getFullName(@Body("data") data: string[],
-  // ) {
-  //   this.logger.log('adding', data[0]);
-  //   this.logger.log('adding', data[1]);
-  //   return this.appService.getFullName(data);
-  // }
   @MessagePattern('summation')
   async getRedisSummation(data: number[]) {
     this.logger.log('adding', data[0]);
