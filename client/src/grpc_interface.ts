290702416af376216f0b6303c3b27b48538aaa5a
abdullah.wasim <abdullahwasim42@gmail.com>
2021-12-05 02:41:43 +0600

    interface, option proto added

---
+++ client/src/grpc_interface.ts
@@ -0,0 +1,8 @@
+import { Observable } from "rxjs";
+
+interface INumberArray {
+    data: number[];
+  }
+  export interface IGrpcService {
+    getMultiplication(data: INumberArray):Observable<any>;
+  }
