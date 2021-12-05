392a706fc36b8edccfba0e59a42c4d907e0986e1
abdullah.wasim <abdullahwasim42@gmail.com>
2021-12-06 01:35:44 +0600

    code reformatted

--- client/src/app.grpc.service.ts
+++ client/src/app.grpc.service.ts
@@ -1,7 +1,5 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import {
-  Client, ClientGrpc
-} from '@nestjs/microservices';
+import { Client, ClientGrpc } from '@nestjs/microservices';
 import { grpcOptions } from './grpc_options';
 import { IGrpcService } from './grpc_interface';
 
@@ -9,7 +7,6 @@ const logger = new Logger('Main');
 
 @Injectable()
 export class AppGrpcService implements OnModuleInit {
-  
   @Client(grpcOptions)
   private client: ClientGrpc;
   private grpcService: IGrpcService;
@@ -17,9 +14,11 @@ export class AppGrpcService implements OnModuleInit {
     this.grpcService = this.client.getService<IGrpcService>('AppController');
   }
   async getMultiplication(data: number[]) {
-    console.log('getMultiplication', data)
-    const result = await this.grpcService.getMultiplication({data}).toPromise();
-    console.log('getMultiplication result', result)
+    console.log('getMultiplication', data);
+    const result = await this.grpcService
+      .getMultiplication({ data })
+      .toPromise();
+    console.log('getMultiplication result', result);
     console.log(result);
     return result;
   }
