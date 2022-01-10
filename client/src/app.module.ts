d53aec59af18f8f35a249ce68c3fed5af1817776
abdullah.wasim <abdullahwasim42@gmail.com>
2022-01-10 13:51:43 +0600

    added bull module in client app

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppGrpcService } from './app.grpc.service';
 import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, AppGrpcService],
 })
